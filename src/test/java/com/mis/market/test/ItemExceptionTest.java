e3db83d52022489224daba2c4ce888fa250183a7
zhaoyanbin <zhaoyanbin@lsh123.com>
2016-12-02 11:33:57 +0800

    修改case

--- src/test/java/com/mis/market/test/ItemExceptionTest.java
+++ src/test/java/com/mis/market/test/ItemExceptionTest.java
@@ -68,7 +68,7 @@ import Basic.Constant;
 import Basic.Log;
 import Basic.Login;
 
-public class ItemExceptionTest /*extends BasicDriver*/ {
+public class ItemExceptionTest /* extends BasicDriver */ {
 	public WebDriver driver;
 
 	// String baseUrl="http://qa.market-mis.wmdev2.lsh123.com";
@@ -76,22 +76,46 @@ public class ItemExceptionTest /*extends BasicDriver*/ {
 	public void itemExceptionTest() throws Exception {
 		ItemExceptionPage itemexception = new ItemExceptionPage(driver);
 		itemexception.exception_btn().click();// 进入在售异常商品页面
-//
-//		String[] strs = new String[] { "所有异常商品", "A档异常商品", "B档异常商品", "C档异常商品", "供货价为0商品", "负毛利商品", "供货价异常", "销售价异常",
-//				"促销价异常" };
+		//
+		// String[] strs = new String[] {"所有异常商品", "A档异常商品", "B档异常商品", "C档异常商品",
+		// "供货价为0商品", "负毛利商品", "供货价异常", "销售价异常",
+		// "促销价异常" };
 		// check物美状态筛选
-         for (int j = 5; j >=0; j--) {
-			
-			
+		for (int j = 5; j >= 0; j--) {
+
 			itemexception.wmstatusCheck1(j);
 		}
-		
+
 		// check筛选
-		 for(int i=8;i>=0;i--){
-		 itemexception.filterCheck(i);
-		 }
-  
- 
+
+		for (int i = 9; i >= 0; i--) {
+			itemexception.filterCheck(i);
+			itemexception.isEquals("");// 这儿需要异常状态名称来调用
+			switch (i) {
+				case 9:
+					itemexception.isEquals("风控置售罄商品");
+				case 8:
+					itemexception.isEquals("促销价异常");
+				case 7:
+					itemexception.isEquals("销售价异常");
+				case 6:
+					itemexception.isEquals("供货价异常");
+				case 5:
+					itemexception.isEquals("负毛利商品");
+				case 4:
+					itemexception.isEquals("供货价为0商品");
+				case 3:
+					itemexception.isEquals("C档售罄商品");
+				case 2:
+					itemexception.isEquals("B档售罄商品");
+				case 1:
+					itemexception.isEquals("A档售罄商品");
+				case 0:
+					itemexception.isEquals("所有异常商品");
+			}
+
+		}
+
 	}
 
 	@BeforeMethod
@@ -99,7 +123,7 @@ public class ItemExceptionTest /*extends BasicDriver*/ {
 		System.setProperty("Webdriver.firefox.bin", "c:\\Program File (X86)\\MozillaFirefox\\firefox.exe");
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		Login.execute(driver,Constant.UserName,Constant.PassWord);
+		Login.execute(driver, Constant.UserName, Constant.PassWord);
 
 		Thread.sleep(5000);
 		Assert.assertTrue(driver.getPageSource().contains("退出"));
